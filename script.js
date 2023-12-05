e28c5d7804ef63e1253db713cc4d22c8ee61a81b
some16 <106182433+some16@users.noreply.github.com>
2023-12-05 14:17:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,16 @@
-function showProfile(data){
-    const profile_data = JSON.stringify(data);
-    document.querySelector('body').innerHTML = profile_data;
-}
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the URL parameter "code"
+    const urlParams = new URLSearchParams(window.location.search);
+    const codeParam = urlParams.get("code");
 
-function getProfile(data) {
-
-    const access_token = data;
-
-    
-    localStorage.setItem('access_token',access_token);
-
-    fetch(`https://api.github.com/user?access_token=${access_token}`)
-    .then(data => data.json())
-    .then(data => showProfile(data))
-    .catch(err => console.error(err));
-}
-
-function getAccessToken() {
-
-    if(access_token = localStorage.getItem('access_token'))
-        getProfile(access_token);
-
-    const clientId = `14a973416657e234da0a`;
-    let code = window.location.search;
-    code = code.replace("?code=", '');
-    console.log('Got the code', code);
-
-    fetch(`http://localhost:5000/${clientId}/${code}`)
-    .then(data => data.json())
-    .then(data => getProfile(data.access_token))
-    .catch(err => console.error(err));
-}
-
-function doWeHaveAccessToken(){
-    if(access_token = localStorage.getItem('access_token')){
-        getProfile(access_token);
+    // Check if the "code" parameter is present in the URL
+    if (codeParam) {
+        // Display the code parameter's value in the "codeContent" div
+        const codeContentElement = document.getElementById("codeContent");
+        codeContentElement.textContent = `Code Parameter: ${codeParam}`;
+    } else {
+        // If the "code" parameter is not found, display a message
+        const codeContentElement = document.getElementById("codeContent");
+        codeContentElement.textContent = "Code Parameter not found in the URL.";
     }
-}
-document.querySelector('button').addEventListener('click', getAccessToken)
-document.addEventListener('DOMContentLoaded', doWeHaveAccessToken);
+});
