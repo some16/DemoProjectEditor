e4d3df6cd297a1cc9521cdde62c529ecde58d539
some16 <106182433+some16@users.noreply.github.com>
2023-12-06 10:32:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,80 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the URL parameter "code"
+    // Get code from url
     const urlParams = new URLSearchParams(window.location.search);
     const codeParam = urlParams.get("code");
 
-    // Check if the "code" parameter is present in the URL
+    // Get token
     if (codeParam) {
-        // Define the Gatekeeper URL
         const gatekeeperUrl = `https://gatekeeper-n0qw.onrender.com/authenticate/${codeParam}`;
-
-        // Send a GET request to the Gatekeeper URL to request the token
+        
         fetch(gatekeeperUrl)
         .then(response => response.json())
         .then(data => {
-            // Display the token on the page
-            const codeContentElement = document.getElementById("codeContent");
-            codeContentElement.textContent = `Token: ${data.token}`;
+            document.cookie = "token=${data.token}; Secure; HttpOnly";
         })
         .catch(error => {
-            // Handle any errors that occur during the fetch request
             console.error("Error:", error);
         });
+
+
+    // If not token found
     } else {
-        // If the "code" parameter is not found, display a message
+
         const codeContentElement = document.getElementById("codeContent");
         codeContentElement.textContent = "Code Parameter not found in the URL.";
     }
 });
+
+
+document.getElementById("getRepos").addEventListener("click", function () {
+    // Retrieve the token from the cookie
+    const cookies = document.cookie.split(";").map((cookie) => cookie.trim());
+    let token = null;
+    for (const cookie of cookies) {
+        const [name, value] = cookie.split("=");
+        if (name === "token") {
+            token = value;
+            break;
+        }
+    }
+
+    if (!token) {
+        console.error("Token not found in the cookie.");
+        return;
+    }
+
+    const apiUrl = "https://api.github.com/user/repos";
+
+    fetch(apiUrl, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        displayRepositories(data);
+    })
+    .catch((error) => {
+        console.error("Error fetching repositories:", error);
+    });
+});
+
+function displayRepositories(repositories) {
+    const repoListElement = document.getElementById("repoList");
+    repoListElement.innerHTML = "";
+
+    if (repositories.length === 0) {
+        repoListElement.textContent = "No repositories found.";
+        return;
+    }
+
+    const ul = document.createElement("ul");
+    repositories.forEach((repo) => {
+        const li = document.createElement("li");
+        li.textContent = repo.name;
+        ul.appendChild(li);
+    });
+
+    repoListElement.appendChild(ul);
+}
