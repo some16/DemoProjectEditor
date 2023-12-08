db5003777f342a95dfb249f7a35f8118003e18ab
some16 <106182433+some16@users.noreply.github.com>
2023-12-08 09:01:16 -0500

    infomatiion

--- scripts/auth/setup.js
+++ scripts/auth/setup.js
@@ -19,21 +19,26 @@ document.addEventListener("DOMContentLoaded", function() {
     const codeParam = urlParams.get("code");
 
     if (codeParam) {
-        console.log("codeParam exists!")
-        console.debug(`Code Param: ${codeParam}`)
+        console.log(`Code Param: ${codeParam}`)
+        console.log(`Fetching token with ${proxyUrl}`);
 
         const proxyUrl = `https://gatekeeper-n0qw.onrender.com/authenticate/${codeParam}`;
 
-        console.log(`Fetching token with ${proxyUrl}`)
         fetch(proxyUrl)
         .then(response => response.json())
         .then(data => {
-            console.log(`Token received: ${data.token}`)
+            console.log(`Token received: ${data.token}`);
             document.cookie = `githubToken=${data.token}`;
             window.location.replace("https://some16.github.io/DemoProjectEditor/editor.html");
         })
         .catch(error => {
             console.error("Error:", error);
         });
+
+        setTimeout(() => {
+        console.log("This is taking longer than expected, it appears the auth proxy has spun down. It will probably be about 15 seconds. Try reloading if nothing happens for a while.");
+        }, 1000); // Adjust the time (in milliseconds) as needed
+
+
     }
 });
